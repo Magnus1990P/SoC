8395c1de6c6ea54257132b3ce789574a13243062
Magnus <services@hackitguys.com>
2025-04-10 22:45:50 +0200

    Update scanner-host_enumeration.sh

--- scanner/scanner-host_enumeration.sh
+++ scanner/scanner-host_enumeration.sh
@@ -3,33 +3,28 @@ OSURL="$1"
 OSUSER="$2"
 OSPASSWD="$3"
 
-SUBFINDER_TARGET_LIST="./subfinder-domain_targets-active.txt"
+OUT_SUBFINDER="./data/OUTPUT-subfinder.jsonl"
+rm -f "$OUT_SUBFINDER"
 
-TF_NAABU="./temp-naabu-targets.txt"
-TF_DNS="./resolvers.txt"
+TF_NAABU="./config/targets-hosts.txt"
+TF_SUBFINDER="./config/targets-domain.txt"
 
-cat $SUBFINDER_TARGET_LIST > $TF_NAABU
+ts=$(date +"%Y-%m-%dT%H:%M:%S%z")
+docker compose run --remove-orphans -it subfinder
 
-############################
-##	HOST ENUMERATION
-SUBFINDER_OUTPUTFILE="./OUTPUT-subfinder.jsonl"
-./subfinder -silent \
-	-dL "$SUBFINDER_TARGET_LIST" \
-	-t 300 -rl 3000 -timeout 1 -recursive \
-	-rL "$TF_DNS" \
-	-nW -oI -oJ -o "$SUBFINDER_OUTPUTFILE" | \
-while read -r JSON; do
+cat "$OUT_SUBFINDER" | \
+while read -r JSON; do 
+	JSON=$(echo $JSON | jq ". += {\"timestamp\": \"$ts\", \"@timestamp\":\"$ts\"}")
 	IDENTIFIER=$(echo "$JSON" | md5sum | awk '{print $1}');
 	if [[ -z "$JSON" ]]; then
 		echo "$IDENTIFIER - $TARGET - NO DETECTIONS";
 	else
 		HNAME=$(jq -r '.host' <<< "$JSON")
-		IP=$(jq -r '.ip' <<< "$JSON")
 		curl -u "$OSUSER:$OSPASSWD" -k -XPOST "$OSURL/scan-dns/_doc/$IDENTIFIER" --json "$JSON" --silent 1>/dev/null
-		echo "$IP" >> $TF_NAABU
 		echo "$HNAME" >> $TF_NAABU
-		echo "HOST DISCOVERED: $HNAME - $IP";
+		echo "HOST DISCOVERED: $HNAME";
 	fi
 done;
-cat "$TF_NAABU" | grep -v '^null:' | sort | uniq > .tmp
+
+cat "$TF_SUBFINDER" "$TF_NAABU" | grep -v '^null:' | sort | uniq > .tmp
 mv .tmp "$TF_NAABU"
