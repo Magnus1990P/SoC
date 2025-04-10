7f9c50f0d6bf3984fa7a6e09ee16455d96e17af2
Magnus <services@hackitguys.com>
2025-04-10 22:46:28 +0200

    Update scanner-portscan.sh

--- scanner/scanner-portscan.sh
+++ scanner/scanner-portscan.sh
@@ -3,18 +3,15 @@ OSURL="$1"
 OSUSER="$2"
 OSPASSWD="$3"
 
-TF_NAABU="./temp-naabu-targets.txt"
-TF_NUCLEI="./temp-nuclei-targets.txt"
-TF_DNS="./resolvers.txt"
+OUT_NAABU="./data/OUTPUT-naabu.jsonl"
+rm -f "$OUT_NAABU"
 
+TF_NUCLEI="./config/targets-services.txt"
 echo -n "" > $TF_NUCLEI
 
-############################
-##	PORT SCAN
-NAABU_OUTPUTFILE="./OUTPUT-nuclei.jsonl"
-sudo ./naabu -silent -json -c 500 -rate 3000 -retries 1 -sa -wn -cdn -tp 1000 \
-	-list "$TF_NAABU" \
- 	-r "$TF_DNS" -o "$NAABU_OUTPUTFILE"| \
+docker compose run --remove-orphans -it naabu
+
+cat "$OUT_NAABU" | grep -v '^null:' | \
 while read -r JSON; do
 	IDENTIFIER=$(echo "$JSON" | md5sum | awk '{print $1}');
 	if [[ -z "$JSON" ]]; then
@@ -29,5 +26,6 @@ while read -r JSON; do
 		echo "PORT DISCOVERED: $HNAME - $IP - $PORT";
 	fi
 done;
+
 cat "$TF_NUCLEI" | grep -v '^null:' | sort | uniq > .tmp
 mv .tmp "$TF_NUCLEI"
